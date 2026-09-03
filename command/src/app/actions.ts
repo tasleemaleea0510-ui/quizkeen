5621c13b1dfbd28d02f67e392c9296fab966137b
tasleemaleea0510-ui <tasleemaleea0510@gmail.com>
2026-09-03 17:23:23 +0200

    batch 93 use server

--- command/src/app/actions.ts
+++ command/src/app/actions.ts
@@ -1,3 +1,4 @@
+"use server";
 import { prisma } from "@/lib/prisma";
 import { createClient } from "@supabase/supabase-js";
 const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || "", process.env.SUPABASE_SERVICE_ROLE_KEY || "");
